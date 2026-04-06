3a784c1b947fa6322e817bc892c0c7278d59e2b9
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-04-06 13:10:38 +0530

    - Created the "SecurityConfig" class to use the "basic authentication" rather than the "form-based authentication" which is the default one.

---
+++ src/main/java/com/learning/spring_security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.learning.spring_security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        httpSecurity.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+}
